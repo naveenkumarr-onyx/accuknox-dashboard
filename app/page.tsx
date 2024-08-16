d07b5e357f048261b03b226d5611871bc8992dec
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2024-08-16 13:33:47 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+import dynamic from "next/dynamic";
 import Dashboard from "./components/categories/Dashboard";
 import Navbar from "./components/navbar/Navbar";
 
 export default function Home() {
+  const NoSSR = dynamic(() => import("./components/categories/Dashboard"), {
+    ssr: false,
+  });
   return (
     <div>
-      <Dashboard />
+      <NoSSR />
     </div>
   );
 }
